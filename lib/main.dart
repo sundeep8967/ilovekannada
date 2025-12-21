5ac8b0a5078bfe306b947b000624aa1b9fe3362b
sundeep8967 <sundeep8967@gmail.com>
2025-12-22 02:07:17 +0530

    feat: Implement core application features, update UI components, API routes, database queries, and integrate mobile platform files.

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'I Love Kannada',
+      title: 'i Love Kannada',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const AppRoot(),
